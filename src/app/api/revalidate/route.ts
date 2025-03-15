f698956dbd5deaeaadaf1f673560563daa3a708d
Cristian Prodius <114479945+CristianProdius@users.noreply.github.com>
2025-03-15 18:27:23 +0200

    fuck this hsit

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -1,26 +1,41 @@
-import { NextApiRequest, NextApiResponse } from "next";
+// app/api/revalidate/route.js (or .ts)
+import { NextRequest, NextResponse } from "next/server";
+import { revalidatePath } from "next/cache";
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  const { path, token } = req.query;
+interface RevalidateRequest extends NextRequest {
+  url: string;
+}
+
+interface RevalidateResponse {
+  revalidated?: boolean;
+  message: string;
+}
+
+export async function GET(
+  request: RevalidateRequest
+): Promise<NextResponse<RevalidateResponse>> {
+  const { searchParams } = new URL(request.url);
+  const path: string | null = searchParams.get("path");
+  const token: string | null = searchParams.get("token");
 
   if (token !== process.env.SECRET_TOKEN) {
-    return res.status(401).json({ message: "Invalid token" });
-  } else if (!path || typeof path !== "string" || path.length === 0) {
-    return res.status(401).json({ message: "Path is required" });
+    return NextResponse.json({ message: "Invalid token" }, { status: 401 });
+  } else if (!path || path.length === 0) {
+    return NextResponse.json({ message: "Path is required" }, { status: 401 });
   }
 
   try {
-    await res.revalidate(path);
+    revalidatePath(path);
+
+    return NextResponse.json({
+      revalidated: true,
+      message: `Path ${path} revalidated successfully`,
+    });
   } catch (err) {
     console.error("Error revalidating path:", err);
-    return res.status(500).send("Error revalidating page");
+    return NextResponse.json(
+      { message: "Error revalidating page" },
+      { status: 500 }
+    );
   }
-
-  return res.status(200).json({
-    revalidated: true,
-    message: `Path ${path} revalidated successfully`,
-  });
 }
